b2f938e6b1556a68cc0d6d747d70866428b10bfa
TacosyHorchata <priosabarca@hotmail.com>
2023-11-27 02:08:42 -0600

    wait list 1.0

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react"
 import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom"
 
-import ErrorBoundary from "./ErrorBoundary";
-
 import OpenBeta from "./OpenBeta"
 import Dashboard from "./Dashboard";
 
@@ -10,14 +8,12 @@ const App = () => {
   
   return (
     <Router>
-      <ErrorBoundary>
         <div className="App">
           <Routes>
             <Route path="/" element={<Dashboard/>}/>
             <Route path="/open-beta" element={<OpenBeta/>}/>
           </Routes>
         </div>
-      </ErrorBoundary>
     </Router>
   );
 }
